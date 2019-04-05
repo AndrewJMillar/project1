79ce7830297c4f2e18385e316d93e2716b6b7d26
Andrew Millar <andrewmillardesign@gmail.com>
2019-04-05 03:13:07 +0000

    task 1 description. function prototype and alphabet array

--- main.c
+++ main.c
@@ -1,1 +1,34 @@
-dfcgvhbjnm
+/* Task 1: Encryption of a message with a rotation
+   cipher given the message and rotation amount.
+   
+   The given message will be: THIS MESSAGE IS 
+   ENCRYPTED
+   The rotaion of the cipher will be: 1
+   
+   For the encryption to occur, it needs to perform
+   a series of tasks-
+    1. A function prototype needs to be generated
+       to apply an encryption equation to rotate
+       the cipher.
+    2. A key needs to be generated to convert each 
+       letter of the alphabet into a number from 
+       0 to 25 using a string array.
+    3. The message will next be intup into the 
+       compiler.
+    4. The function will be called and each letter 
+       of the message will be encypted individually.
+    5. The encrypted message will be printed. */
+
+#include <stdio.h>
+
+int encryption(int m, int k) //function prototype for encryption
+
+
+int main(void) {
+    
+    char alphabet[27] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '\0'}; // string array of alphabet
+    
+    
+    
+    return 0;
+}
