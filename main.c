68c33bf1f5a1df9891d5992bbd46c859dd71b134
Andrew Millar <andrewmillardesign@gmail.com>
2019-04-12 02:39:01 +0000

    Task 1 completed

--- main.c
+++ main.c
@@ -1,57 +1,33 @@
-/* Task 1: Encryption of a message with a rotation
-   cipher given the message and rotation amount.
-   
-   The given message will be: THIS MESSAGE IS 
-   ENCRYPTED
-   The rotaion of the cipher will be: 1
-   
-   For the encryption to occur, it needs to perform
-   a series of tasks-
-    1. A function prototype needs to be generated
-       to apply an encryption equation to rotate
-       the cipher.
-    2. A key needs to be generated to convert each 
-       letter of the alphabet into a number from 
-       0 to 25 using a string array.
-    3. The message will next be intup into the 
-       compiler.
-    4. The function will be called and each letter 
-       of the message will be encypted individually.
-    5. The encrypted message will be printed. */
-
 #include <stdio.h>
 
+int main(void)
+{
+   FILE *input;
+   
+   input = fopen("input.txt", "r");
+   
+   
+   while(feof(input) == 0) {
+              
+      char c;  // declared character 'c' will change to the next charater in input.txt after each loop  
+     
+      int key=1; // the key is the number of times each letter is rotated through the alphabet.
+      
+      fscanf(input, "%c", &c); // this scans each letter of input.txt and stores them at the address of 'c'
+      
+         if ((c >= 'A') && (c <= 'Z')) { // Since its only the letters that are being rotated, this isolates the roation to just the letters of the alphabet 
+        
+         c = c + key;
+        
+         if (c > 'Z') {
+            
+         c = c % 26;
+        }
+        
+  }
 
-int main() {
-    
-    char alphabet[27] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', '\0'}; // string array of alphabet
-    
-   FILE *text;
-   
-   text = fopen("input.txt", "r");
-   
-   while(feof(text) == 0) {
-       
-       char c;
-       
-       int key=1;
-       
-       fscanf(text, "%c", &c);
-       
-       if((c >= A) && (c <= Z)) {
-           
-           c = c + key;
-           
-           if(c > Z) {
-               
-               c = c % 26;
-           }
-       }
-       printf("%c", c);
+      printf("%c", c);
+      
    }
-   
-   
-    
-    
-    return 0;
-}
+ 
+ }
