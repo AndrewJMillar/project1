3cdd6496dd5c712f511120acd898937ad08767ca
Andrew Millar <andrewmillardesign@gmail.com>
2019-04-25 08:35:00 +0000

    task 4 completed (needs commenting still)

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 void task1(void); //THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
 void task2(void); //WKH TXLFN EURZQ IRA MXPSV RYHU WKH ODCB GRJ
-void task3(void);
-void task4(void);
+void task3(void); //THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
+void task4(void); //ZIT JXOEA WKGVF YGB PXDHL GCTK ZIT SQMN RGU
 
 
 int main()   {
@@ -11,33 +11,41 @@ int main()   {
     int task;
     
     
-    printf("Task1= 1\n");
-    printf("Task2= 2\n");
-    printf("Task3= 3\n");
-    printf("Task4= 4\n");
+    printf("   -Task1 = 1-\n");
+    printf("   -Task2 = 2-\n");
+    printf("   -Task3 = 3-\n");
+    printf("   -Task4 = 4-\n");
     printf("Enter a number to run a task: ");
     
     scanf("%d", &task);
     
     switch(task)    {
         case 1:
+            printf("...YOU HAVE CHOSEN TASK 1!!!...\n");
             task1();
+            printf("\n");
             break;
         case 2:
+            printf("...YOU HAVE CHOSEN TASK 2!!!...\n");
             task2();
+            printf("\n");
             break;
         case 3:
+            printf("...YOU HAVE CHOSEN TASK 3!!!...\n");
             task3();
+            printf("\n");
             break;
         case 4:
-            printf("you have chosen task 4\n");
+            printf("...YOU HAVE CHOSEN TASK 4!!!...\n");
+            task4();
+            printf("\n");
             break;
         default:
             printf("\n...AN ERROR OCCURED...");
-    }
+}
     
     return 0;
- }
+}
  
 void task1(void)    {   //this calls the function "task1"
      
@@ -56,13 +64,13 @@ void task1(void)    {   //this calls the function "task1"
                 c = c + key; //this takes the current value of c and ADDS the key. It then makes the total the new c value.
                 if (c > 'Z') { // this determines whether the value of c exceeds the the number letters in the alphabet (26)
                     c = c - 26; // if the value of c exceeds 26, to loop back to A, the value of c is deducted by 26
-        }   
-  }
+}   
+}
     printf("%c", c); // this prints the value of c after the rotation cypher has been applied
       
-   }
+}
     
-    }
+}
     
  void task2(void)    { // this calls the function "task2"
      
@@ -81,8 +89,8 @@ void task1(void)    {   //this calls the function "task1"
                 c = c - key; //this takes the current value of c and DEDUCTS the key. It then makes the total the new c value.
                 if (c < 'A') { // this determines whether the value of c exceeds the the number letters in the alphabet (26)
                     c = c + 26; // if the value of c exceeds 26, to loop back to A, the value of c has 26 added to it
- }   
- }
+}   
+}
     printf("%c", c); // this prints the value of c after the rotation cypher has been applied
       
 }
@@ -99,7 +107,6 @@ void task3(void)    {
     while(feof(input) == 0) { // this begins a loop which reads the first charater of "input.txt", then completes the loop, then reads the second character etc
     
         char c;  // declared character 'c' will change to the next charater in input.txt after each loop 
-        
         
         fscanf(input, "%c", &c); // this scans each letter of input.txt and stores them at the address of 'c'
         
@@ -244,7 +251,159 @@ void task3(void)    {
     
 }
 
-
+void task4(void)    {
+    
+    
+    FILE *input; // this is a pointer to file "input"
+   
+    input = fopen("input.txt", "r"); // this makes the data in "input.txt" to "input"
+    
+    while(feof(input) == 0) { // this begins a loop which reads the first charater of "input.txt", then completes the loop, then reads the second character etc
+    
+        char c;  // declared character 'c' will change to the next charater in input.txt after each loop 
+        
+        fscanf(input, "%c", &c); // this scans each letter of input.txt and stores them at the address of 'c'
+        
+          
+            if (c == 'Q')   {
+                c = 'A';
+                printf("%c", c);
+                c = 'Q';
+}
+            if (c == 'W')   {
+                c = 'B';
+                printf("%c", c);
+                c = 'W';
+}
+            if (c == 'E')   {               
+                c = 'C';
+                printf("%c", c);
+                c = 'E';
+}
+            if (c == 'R')   {                
+                c = 'D';
+                printf("%c", c);
+                c = 'R';
+}
+            if (c == 'T')   {                
+                c = 'E';
+                printf("%c", c);
+                c = 'T';
+}
+            if (c == 'Y')   {                
+                c = 'F';
+                printf("%c", c);
+                c = 'Y';
+}
+            if (c == 'U')   {               
+                c = 'G';
+                printf("%c", c);
+                c = 'U';
+}
+            if (c == 'I')   {                
+                c = 'H';
+                printf("%c", c);
+                c = 'I';
+}
+            if (c == 'O')   {                
+                c = 'I';
+                printf("%c", c);
+                c = 'O';
+}
+            if (c == 'P')   {                
+                c = 'J';
+                printf("%c", c);
+                c = 'P';
+}
+            if (c == 'A')   {              
+                c = 'K';
+                printf("%c", c);
+                c = 'A';
+}
+            if (c == 'S')   {               
+                c = 'L';
+                printf("%c", c);
+                c = 'S';
+}
+            if (c == 'D')   {                
+                c = 'M';
+                printf("%c", c);
+                c = 'D';
+}
+            if (c == 'F')   {                
+                c = 'N';
+                printf("%c", c);
+                c = 'F';
+}
+            if (c == 'G')   {                
+                c = 'O';
+                printf("%c", c);
+                c = 'G';
+}
+            if (c == 'H')   {               
+                c = 'P';
+                printf("%c", c);
+                c = 'H';
+}
+            if (c == 'J')   {
+                c = 'Q';
+                printf("%c", c);
+                c = 'J'; 
+}
+            if (c == 'K')   {               
+                c = 'R';
+                printf("%c", c);
+                c = 'K';
+}
+            if (c == 'L')   {                
+                c = 'S';
+                printf("%c", c);
+                c = 'L';
+}
+            if (c == 'Z')   {               
+                c = 'T';
+                printf("%c", c);
+                c = 'Z';
+}
+            if (c == 'X')   {                
+                c = 'U';
+                printf("%c", c);
+                c = 'X';
+}
+            if (c == 'C')   {                
+                c = 'V';
+                printf("%c", c);
+                c = 'C';
+}
+            if (c == 'V')   {                
+                c = 'W';
+                printf("%c", c);
+                c = 'V';
+}
+            if (c == 'B')   {               
+                c = 'X';
+                printf("%c", c);
+                c = 'B';
+}
+            if (c == 'N')   {                
+                c = 'Y';
+                printf("%c", c);
+                c = 'N';
+}
+            if (c == 'M')   {               
+                c = 'Z';
+                printf("%c", c);
+                c = 'M';
+}  
+            else if ((c != 'A') && (c != 'B') && (c != 'C') && (c != 'D') && (c != 'E') && (c != 'F') && (c != 'G') && (c != 'H') && (c != 'I') && (c != 'J') 
+                    && (c != 'K') && (c != 'L') && (c != 'M') && (c != 'N') && (c != 'O') && (c != 'P') && (c != 'Q') && (c != 'R') && (c != 'S') && (c != 'T') 
+                    && (c != 'U') && (c != 'V') && (c != 'W') && (c != 'X') && (c != 'Y') && (c != 'Z'))  {
+                 printf("%c", c);
+}          
+   
+}
+    
+}
 
  
  
